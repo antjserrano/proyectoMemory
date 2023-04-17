9bf961a1681542daad6a1d87a9a1c492ee58eba0
antjserrano <aserrano@itpfp.com>
2023-04-17 12:16:36 +0200

    Update memory.cs

--- memory.cs
+++ memory.cs
@@ -7,7 +7,7 @@ public class memory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(escort);
+        
     }
 
     // Update is called once per frame
